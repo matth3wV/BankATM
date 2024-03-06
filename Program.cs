13302ac67a3d91e6c58d50709a062720c0e9320f
Matthew <matth3wV@users.noreply.github.com>
2024-03-05 23:57:24 -0500

    project files
    Spent days fighting EntityFramework and got nowhere.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add a user to db
+            using (ATMContext context = new ATMContext())
+            {
+                var x = new atmUsers(login = "JimF", pincode = "12345", type = "admin");
+                context.Atm_users.Add(x);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ user.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using System.Data.Common;
+using System.Data.Entity;
+
+
+namespace ATMApp
+{
+    class atmUsers
+    {
+        public int id { get; set; }
+        public string login { get; set; }
+        public string pincode { get; set; }
+        public string userType { get; set; }
+
+    }
+
+
+    public class ATMContext : DBContext
+    {
+        static readonly string connectionString = "Server=localhost;User ID=root;Password=password321; Database=atm_users";
+        public DbSet<atmUsers> atm_users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+    
+    }
+
+}
